81a30e41b6b40059d84efd56fe5ed77b40250095
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-12-03 17:51:52 +0800

    1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 1.0.1
+- Allow player to shiftKeyDown + F9 -> Config reloading
--- src/main/java/me/kall/whatversioning/WhatVersioning.java
+++ src/main/java/me/kall/whatversioning/WhatVersioning.java
@@ -19,38 +19,9 @@ import java.util.concurrent.ThreadLocalRandom;
 public final class WhatVersioning {
     public static final String MOD_ID = "whatversioning";
 
-    public static final JsonConfig CONFIG = JsonConfig.create(MOD_ID, "1")
-            .put("CustomVersioning", Lists.newArrayList("Minecraft 26H2",
-                    "Minecraft 114.514",
-                    "Minecraft 2/3/4/5",
-                    "Minecraft blackwell",
-                    "Minecraft operation black ice",
-                    "Minecraft mojang is retarded",
-                    "Minecraft 26.2",
-                    "Minecraft MoForce 576.08",
-                    "Minecraft 4™",
-                    "Minecraft CopperLake",
-                    "Minecraft i25-18900kx",
-                    "Minecraft 42.7.04",
-                    "Minecraft 0.114.5",
-                    "Minecraft CreateOS 1.0.114.6",
-                    "Minecraft 845",
-                    "Minecraft UI 12.5 Enhanced Edition",
-                    "Minecraft：第12话 我最好的史蒂夫",
-                    "Minecraft: 终",
-                    "Minecraft: It's MyGO!!!!!",
-                    "Minecraft KB5063878",
-                    "Minecraft Y16S1",
-                    "Minecraft 2025 Ver.CN1.14.5G",
-                    "Minecraft 9.9.25-114514",
-                    "Minecraft 1/2",
-                    "Minecraft Studio 581.57"
-            ))
-            .initialize();
+    public static List<String> VERSIONS;
 
-    public static final List<String> VERSIONS = CONFIG.getList("CustomVersioning", String.class);
-
-    public static String CURRENT = VERSIONS.get(ThreadLocalRandom.current().nextInt(VERSIONS.size()));
+    public static String CURRENT;
 
     public static final KeyMapping CHANGE_TITLE = new KeyMapping(
             "key.whatversioning.change_title",
@@ -58,6 +29,44 @@ public final class WhatVersioning {
             GLFW.GLFW_KEY_F9,
             "category.whatversioning.title"
     );
+
+    static {
+        loadConfig();
+    }
+
+    private static void loadConfig() {
+        JsonConfig config = JsonConfig.create(MOD_ID, "1")
+                .put("CustomVersioning", Lists.newArrayList(
+                        "Minecraft 26H2",
+                        "Minecraft 114.514",
+                        "Minecraft 2/3/4/5",
+                        "Minecraft blackwell",
+                        "Minecraft operation black ice",
+                        "Minecraft mojang is retarded",
+                        "Minecraft 26.2",
+                        "Minecraft MoForce 576.08",
+                        "Minecraft 4™",
+                        "Minecraft CopperLake",
+                        "Minecraft i25-18900kx",
+                        "Minecraft 42.7.04",
+                        "Minecraft 0.114.5",
+                        "Minecraft CreateOS 1.0.114.6",
+                        "Minecraft 845",
+                        "Minecraft UI 12.5 Enhanced Edition",
+                        "Minecraft：第12话 我最好的史蒂夫",
+                        "Minecraft: 终",
+                        "Minecraft: It's MyGO!!!!!",
+                        "Minecraft KB5063878",
+                        "Minecraft Y16S1",
+                        "Minecraft 2025 Ver.CN1.14.5G",
+                        "Minecraft 9.9.25-114514",
+                        "Minecraft 1/2",
+                        "Minecraft Studio 581.57"
+                ))
+                .initialize();
+        VERSIONS = config.getList("CustomVersioning", String.class);
+        CURRENT = VERSIONS.get(ThreadLocalRandom.current().nextInt(VERSIONS.size()));
+    }
 
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT, modid = MOD_ID)
     public static final class ModEvents {
@@ -72,8 +81,13 @@ public final class WhatVersioning {
         @SubscribeEvent
         public static void tick(TickEvent.ClientTickEvent event) {
             if (event.phase.equals(TickEvent.Phase.END) && CHANGE_TITLE.consumeClick()) {
+                Minecraft minecraft = Minecraft.getInstance();
+                if (minecraft.player != null && minecraft.player.isShiftKeyDown()) {
+                    loadConfig();
+                    System.out.println("WhatVersioning config has reloaded.");
+                }
                 CURRENT = VERSIONS.get(ThreadLocalRandom.current().nextInt(VERSIONS.size()));
-                Minecraft.getInstance().updateTitle();
+                minecraft.updateTitle();
             }
         }
     }
